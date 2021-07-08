ead15a2ae269ad6248c4877c05c09e23f4ad0c1b
Alex Desyatnik <alexdesyatnik@outlook.com>
2021-07-08 17:34:53 +0300

    Project Stage 2 is done

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -3,6 +3,11 @@
 import { Command } from 'commander/esm.mjs';
 const program = new Command();
 
-program.version('0.0.1');
-program.description('Compares two configuration files and shows a difference.');
+program
+    .version('0.0.1')
+    .description('Compares two configuration files and shows a difference.')
+    .argument('<filepath1>')
+    .argument('<filepath2>')
+    .option('-f, --format [type]', 'output format');
+
 program.parse(process.argv);
